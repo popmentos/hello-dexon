3f7d32231b799b912b7b6582d206c80a1a1996d4
Wayne Chiu <chaoweichiu@gmail.com>
2018-12-20 09:02:44 +0800

    DEXON Derivation Path Explanation
    Add description for why using "m/44'/237'/0'/0/"

--- truffle.js
+++ truffle.js
@@ -21,8 +21,32 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     testnet: {
-      provider: new HDWalletProvider(mnemonic,
-        "http://testnet.dexon.org:8545", 0, 1, true, "m/44'/237'/0'/0/"),
+      provider: new HDWalletProvider(
+        mnemonic,
+        /*
+        * mnemonic is from BIP39
+        * BIP39 is the method for deriving private keys from mnemonic words.
+        * With different derivation path,
+        * we can generate different private keys from the same set of mnemonic word
+        */
+        "http://testnet.dexon.org:8545",
+        0,
+        1,
+        true,
+        "m/44'/237'/0'/0/",
+        /*
+        * Derivation Path <= "m/44'/237'/0'/0/"
+        * 
+        * BIP44 defines how what derivation path should be like,
+        * and it relies on coin type.
+        * 
+        * Ethereum is registered as 60 『"m/44'/60'/0'/0/"』,
+        * while DEXON is registered as 237 『"m/44'/237'/0'/0/"』.
+        * 
+        * Therefore, given the same set of mnemonic words,
+        * the addresses for Ethereum and DEXON are different.
+        */
+      ),
       network_id: "*"
     },
     development: {
